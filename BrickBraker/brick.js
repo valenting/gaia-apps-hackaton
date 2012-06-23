17da42a6ba15ae7dc72b41bbcbf8b4eed313e91e
Gabriel Ivanica <ivanicagabriel@gmail.com>
2012-06-23 18:54:40 +0300

    adfadf

--- BrickBraker/brick.js
+++ BrickBraker/brick.js
@@ -1,37 +1,49 @@
-var canvas_margin = 20;
-var width = 640;
-var height = 480;
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext( '2d' );
-var playerX = 100;
-var	playerY = 100;
-var speed = 2;
+var CTX = null;
 
-window.addEventListener("keydown",doKeyDown,true);
 
-function mainLoop(){
-	context.fillStyle = "yellow";
-context.fillRect(0,0,320,480);
-context.fillStyle = "black";
-context.fillRect(playerX,playerY,50,10);
-}
+$(window).load( function (e) {
+	CTX = document.getElementById("canvas").getContext('2d'); 
+	Canvas.init();
+});
 
-function doKeyDown(event) {
-	switch (event.keyCode) {
-			/*case 38: //Up arrow
-					speedY = -speed;
-					speedX=0;
-				break;
-			case 40: //Down arrow
-					speedY = speed;
-					speedX = 0;
-				break;*/
-			case 37: /*Left arrow*/
-					playerX -= speed;
-				break;
-			case 39: /*Right arrow*/
-					playerX += speed;
-										
-				break;
+
+ 
+
+
+var Canvas = {
+
+	playerX : 100,
+	playerY : 600,
+	speed : 5,
+	
+
+	init : function () {
+		setInterval(this.draw , 10);
+		console.log(this);		
+		//window.addEventListener('keypress', this.doKeyDown, false);
+		window.onkeypress = Canvas.doKeyDown;
+	},
+
+	draw : function () {
+		CTX.fillStyle = "yellow";
+		CTX.fillRect(0, 0, 480, 800);
+		CTX.fillStyle = "black";
+		CTX.fillRect(Canvas.playerX, Canvas.playerY, 100, 20);
+	},
+
+	doKeyDown : function (e) {
+		switch (e.keyCode) {
+				case 37: /*Left arrow*/
+						Canvas.playerX -= Canvas.speed;
+					break;
+				case 39: /*Right arrow*/
+						Canvas.playerX += Canvas.speed;
+											
+					break;
+		}
 	}
+	
 }
+
+
+	//window.onload = Canvas.init;
