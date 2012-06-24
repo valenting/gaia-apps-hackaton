fe8e6264a368c011ceb74ad554d66216bb78748a
Alin-Alexandru Turlac <alin.turlac@gmail.com>
2012-06-24 12:22:43 +0300

    Enabled upload support

--- MemeGen/js/editnow.js
+++ MemeGen/js/editnow.js
@@ -45,3 +45,38 @@
 			canvas.height=500;
 			context.drawImage(centralimage,0,0,500,500);
 		}
+		
+
+		function share(){
+			try {
+				var img = canvas.toDataURL('image/jpeg', 0.9).split(',')[1];
+			} catch(e) {
+				var img = canvas.toDataURL().split(',')[1];
+			}
+			// open the popup in the click handler so it will not be blocked
+			var w = window.open();
+			w.document.write('Uploading...');
+			// upload to imgur using jquery/CORS
+			// https://developer.mozilla.org/En/HTTP_access_control
+			$.ajax({
+				url: 'http://api.imgur.com/2/upload.json',
+				type: 'POST',
+				data: {
+					type: 'base64',
+					// get your key here, quick and fast http://imgur.com/register/api_anon
+					key: 'fc862c0aa18287070c439c8c0f039bca',
+					name: 'meme.jpg',
+					title: 'memegen',
+					caption: 'Created with the hackaton Meme Generator!',
+					image: img
+				},
+				dataType: 'json'
+			}).success(function(data) {
+				var x=data['upload']['links']['imgur_page'];
+				w.close();
+				document.getElementById("imageURL").value=x;
+			}).error(function() {
+				alert('Could not reach api.imgur.com. Sorry :(');
+				w.close();
+			});
+}
